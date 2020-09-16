e8e46a4937b57f8fde2b0e1539dff1ce57228130
arthurktripp <arthur.k.tripp@gmail.com>
2020-09-16 00:33:29 -0700

    rebuilt logical foundation of value storage

--- calc.js
+++ calc.js
@@ -1,117 +1,176 @@
+// declare global variables
+var firstString = "0";
+var secondString = "0";
+var operandItteration = 1;
 var firstNum = 0;
-var secondNum;
-var operator;
-var answer;
-var displayString = 0;
-var displayValue;
+var secondNum = 0;
+var key;
+var x = 0;
 var displayLength = 0;
-var x;
+var displayValue;
+var answer = 0;
 
 
+// STORES INPUT AS ONE OF TWO STRINGS
+const writeString = function() {
+    switch (operandItteration) {
+        case 1:
+            if (firstString.length <= 9){
+                firstString += key;
+                firstNum = parseFloat(firstString);
+                break;
+            } else {break};
+        case 2:
+            if (secondString.length <= 9) {
+                secondString += key;
+                secondNum = parseFloat(secondString);
+                break;
+            } else {break};
+        case 3:
+            if (secondString.length <= 9){
+                firstString = answer.toString();
+                firstNum = answer;
+                secondString += key;
+                secondNum = parseFloat(secondString);
+                break;
+            } else {break};
+    }   
+}
 
-
-
-// updates the calculator display, with a nine-digit max limitation built in like an old calculator.
+// UPDATES THE CALCULATOR DISPLAY TO REFLECT CURRENT NUMBER
 const updateDisplay = function() {
-    if (displayLength < 9) { 
-        displayValue = parseFloat(displayString);
-        document.getElementById("read").innerHTML = (displayValue);
-        x = displayValue.toString();
-        displayLength = x.length;
+    if (operandItteration == 1) {
+        if (displayLength < 9) { 
+            displayValue = parseFloat(firstString);
+            if (displayValue == Math.floor(displayValue)) {
+                document.getElementById("readout").innerHTML = (displayValue + '.');
+            } else {
+                document.getElementById("readout").innerHTML = (displayValue);
+            }
+            x = displayValue.toString();
+            displayLength = x.length;
+       } 
+    } else if (operandItteration == 2) {
+        if (displayLength < 9) {
+            displayValue = parseFloat(secondString);
+            if (displayValue == Math.floor(displayValue)) {
+                document.getElementById("readout").innerHTML = (displayValue + '.');
+            } else {
+                document.getElementById("readout").innerHTML = (displayValue);
+            }
+            x = displayValue.toString();
+            displayLength = x.length;
+        }
+        } else if (operandItteration == 3) {
+            if (displayLength < 9) {
+                displayValue = parseFloat(firstString);
+                if (displayValue == Math.floor(displayValue)) {
+                    document.getElementById("readout").innerHTML = (displayValue + '.');
+                } else {
+                    document.getElementById("readout").innerHTML = (displayValue);
+                }
+                x = displayValue.toString();
+                displayLength = x.length;
+                
+            }
+        }
     }
-}
 
-// eventlisteners for the number keys
-const zeroKey = document.getElementById("zero").addEventListener("click",
-    function() { 
-        displayString += this.dataset.key;
-        updateDisplay()
+
+
+
+
+
+//CLEAR KEY
+const clearKey = document.getElementById("clear").addEventListener("click",
+    function() {
+        firstString = "0";
+        secondString = "0";
+        operandItteration = 1;
+        firstNum = 0;
+        secondNum = 0;
+        x = 0;
+        displayLength = 0;
+        displayValue;
+        updateDisplay();      
     });
-const oneKey = document.getElementById("one").addEventListener("click",
-    function() { 
-        displayString += this.dataset.key;
-        updateDisplay()
+
+
+// PLUS KEY
+const plusKey = document.getElementById('plus').addEventListener("click",
+    function() {
+        switch (operandItteration) {
+            case 1:
+                operator = '+';
+                operandItteration = 2;
+                break;
+            case 2:
+                operator = '+'
+                calculate();
+                firstString = answer.toString();
+                secondNum = 0;
+                secondString = '0';
+                operandItteration = 3;
+                writeString();
+                updateDisplay();
+                break;
+            case 3:
+                if (key == 'equals') {
+                    secondNum = 0;
+                    secondString = 0;
+                }
+                operator = '+'
+                calculate();
+                firstString = answer.toString();
+                firstNum = answer;
+                secondNum = 0;
+                secondString = '0';
+                writeString();
+                updateDisplay();
+                break;
+        }
     });
-const twoKey = document.getElementById("two").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const threeKey = document.getElementById("three").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const fourKey = document.getElementById("four").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const fiveKey = document.getElementById("five").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const sixKey = document.getElementById("six").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const sevenKey = document.getElementById("seven").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const eightKey = document.getElementById("eight").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
-const nineKey = document.getElementById("nine").addEventListener("click",
-function() { 
-    displayString += this.dataset.key;
-    updateDisplay()
-});
+
+// EQUALS KEY
+const equalsKey = document.getElementById('equals').addEventListener("click",
+    function() {
+        switch (operandItteration) {
+            case 1:
+                break;
+            case 2:
+                calculate();
+                firstString = 0 + answer.toString();
+                key = 'equals'
+                operandItteration = 3;
+                writeString();
+                updateDisplay();
+                break;
+            case 3:
+                calculate();
+                firstString = 0 + answer.toString();
+                key = 'equals'
+                operandItteration = 3;
+                writeString();
+                updateDisplay();
+                break;
+        }
+    })
 
 
-// functions for each operator
-const addition = function(){
-    answer = (firstNum + secondNum).toPrecision(9);
-}
-const subtraction = function(){
-    answer = (firstNum - secondNum).toPrecision(9);
-}
-const multiplication = function(){
-    answer = (firstNum * secondNum).toPrecision(9);
-}
-const division = function() {
-    answer = (firstNum / secondNum).toPrecision(9);
-}
-
-// This function should be initiated when I click EQUALS, or an equivalent event occurs (such as selecting an operator after secondNum has been stored.)
+// CALCULATION FUNCTION
 const calculate = function() {
-    
-    
-    /* THIS CODE WAS A PLACEHOLDER FOR RECEIVING INPUT VIA PROMPTS
-    
-    firstNum = parseFloat(prompt('first number?'));
-    operator = prompt('operator?', '+ - * /');
-    secondNum = parseFloat(prompt('second number?'));
-    */
-
-    // This function should be initiated when I click EQUALS;
     switch (operator) {
         case '+':
-            addition();
+            answer = firstNum + secondNum;
             break;
         case "-":
-            subtraction();
+            answer = firstNum - secondNum;
             break;
         case "*":
-            multiplication();
+            answer = firstNum * secondNum;
             break;
-        case "/":
-            division();
+        case "/": // Needs an out if secondNum is zero
+            answer = firstNum / secondNum;
             break;
         default:
             answer = "That's not right" 
@@ -119,3 +178,131 @@ const calculate = function() {
     return answer;
 }
 
+
+
+
+
+
+
+
+
+
+// eventlisteners for the number keys
+const zeroKey = document.getElementById("zero").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+
+const oneKey = document.getElementById("one").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+
+const twoKey = document.getElementById("two").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const threeKey = document.getElementById("three").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const fourKey = document.getElementById("four").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const fiveKey = document.getElementById("five").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const sixKey = document.getElementById("six").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const sevenKey = document.getElementById("seven").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const eightKey = document.getElementById("eight").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+const nineKey = document.getElementById("nine").addEventListener("click",
+    function() { 
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+    });
+
+const decimalKey = document.getElementById('decimal').addEventListener("click",
+    function() {
+        if ( (operandItteration == 1 && (firstString.indexOf('.') >= 0) ) || (operandItteration == 2 && secondString.indexOf('.') >= 0) ) {
+            return; //kills decimal key once it has been used in current number
+        } else {
+        key = this.dataset.key;
+        if (operandItteration == 3){
+            secondString = '';
+            operandItteration = 2;
+        }
+        writeString();
+        updateDisplay()
+        }
+    });
+    
